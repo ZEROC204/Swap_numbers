fa0439a4e9dcf923e3535c891972e972072fe813
John <76859767+ZEROC204@users.noreply.github.com>
2022-09-29 09:32:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Swapping two numbers")
+num1 = input("Enter any number ( num1 ): ")
+num2 = input("Enter any number ( num2 ): ")
+a = num1
+num1 = num2
+num2 = a
+print(f'The value of num1 before swapping: {a} and value of num2 before swapping: {num1}')
+print(f'The value of num1 after swapping: {num1} and value of num2 before swapping: {num2}')
